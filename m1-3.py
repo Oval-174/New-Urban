9c6369eb531b030285dd7c9acaeba080a437b422
Veniamin Orlenko <vorlenko@d2k.ru>
2024-05-31 17:08:17 +0300

    Restart m1-3

---
+++ m1-3.py
@@ -0,0 +1,9 @@
+# Практическая работа по уроку "Динамическая типизация"
+name = 'Veniamin'
+print('Name:', name)
+age = 30
+print('Age:', age)
+age += 10
+print('New Age:', age)
+is_student = True
+print('Is Student:', is_student)
