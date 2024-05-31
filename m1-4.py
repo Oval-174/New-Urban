16c00ab79feb3210188b53504d36f36ec746d3c1
Veniamin Orlenko <vorlenko@d2k.ru>
2024-05-31 17:43:03 +0300

    Restart m1-4

---
+++ m1-4.py
@@ -0,0 +1,8 @@
+# Практическая работа по уроку "Организация программ и методы строк."
+my_string = input('Введите произвольную строку: ')
+print('Длина строки:', len(my_string), 'символов')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
