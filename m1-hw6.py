a9bac77fdc8417dff96734f1c81348993eeeaab3
vorlenko <4zik174@mail.ru>
2024-06-22 14:24:13 +0300

    Restart m1-6-1

--- m1-hw6.py
+++ m1-hw6.py
@@ -3,21 +3,21 @@
 # 2 Работа со словарями
 
 my_dict = {"Sasha": 2000, "Masha": 2001, "Katty": 2002}
-print(my_dict)
-print(my_dict["Masha"])
-print(my_dict.get("Vikky"))
+print("Dict:",my_dict)
+print("Existing value:", my_dict["Masha"])
+print("Not existing value:", my_dict.get("Vikky"))
 my_dict["Nick"] = 2003
 my_dict.update({"Bob": 1955})
-print(my_dict)
-print(my_dict.pop("Sasha"))
-print(my_dict)
+print("Modified dictionary:", my_dict)
+print("Deleted value:", my_dict.pop("Sasha"))
+print("Modified dictionary:", my_dict)
 
 # 3 Работа с множествами
 
 my_set = {"Sasha", 1975, "Masha", 1999, "Katty", 2002}
-print(my_set)
+print("Set:", my_set)
 my_set.update(["Katty", "Nick", "Bob", 1565])
-print(my_set)
+print("Modified set:", my_set)
 my_set.discard("Sasha")
-print(my_set)
+print("Modified set:", my_set)
 my_set.discard("Sasha")
