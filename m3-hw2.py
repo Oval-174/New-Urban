443e47534e44203c6ede3d9d76827d196d3aaebc
Veniamin Orlenko <vorlenko@d2k.ru>
2024-07-01 18:16:02 +0300

    Restart m3-2 n/f/2

--- m3-hw2.py
+++ m3-hw2.py
@@ -1,11 +1,6 @@
 # m3-hw2.py "Рассылка писем"
-def send_email(message, recipient, sender = "university.help@gmail.com"):
+def send_email(message, recipient, sender="university.help@gmail.com"):
     if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
         return
-    if message.find('@') == -1 or recipient.find('@') == -1:
-            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-
-
-send_email("1","2",1)
 
