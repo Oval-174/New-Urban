02bf47b53d507f24b741a9c235fc1ee5a9d513d8
Veniamin Orlenko <vorlenko@d2k.ru>
2024-07-24 15:06:39 +0300

    Restart m7-hw2

---
+++ m7-hw2.py
@@ -0,0 +1,23 @@
+# m7-hw1 "Записать и запомнить"
+
+def custom_write(file_name, strings):
+    file = open(file_name, 'w', encoding='utf-8')
+    list_strings = strings
+    __result = {}
+    for i in range(0, len(list_strings)):
+        __result[i + 1, file.tell()] = list_strings[i]
+        file.write(list_strings[i] + '\n')
+    file.close()
+    return __result
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
