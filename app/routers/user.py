edf0827eecb073de45a52b2935283fde7dec880d
vorlenko <4zik174@mail.ru>
2024-10-11 23:39:14 +0300

    Restart m17-hw5 final for m17

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,17 +2,19 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from app.backend.db_depends import get_db
 from typing import Annotated
-from app.models import User
+from app.models import Task, User
 from sqlalchemy import insert, select, update, delete
 from app.schemas import CreateUser, UpdateUser
 from slugify import slugify
 
 router = APIRouter(prefix='/user', tags=['user'])
 
+
 @router.get('/')
 async def all_users(db: Annotated[Session, Depends(get_db)]):
     users = db.scalars(select(User)).all()
     return users
+
 
 @router.get('/user_id')
 async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
@@ -23,6 +25,18 @@ async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
             detail='User was not found'
         )
     return user
+
+
+@router.get('/user_id/tasks')
+async def tasks_by_user_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    user = db.scalar(select(User).where(User.id == user_id))
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User was not found'
+        )
+    tasks = db.scalars(select(Task).where(Task.user_id == user_id)).all()
+    return tasks
 
 
 @router.post('/create')
@@ -40,6 +54,7 @@ async def create_user(db: Annotated[Session, Depends(get_db)], create_user: Crea
         'transaction': 'Successful'
     }
 
+
 @router.put('/update')
 async def update_user(db: Annotated[Session, Depends(get_db)], user_id: int,
                           update_user: UpdateUser):
@@ -49,7 +64,7 @@ async def update_user(db: Annotated[Session, Depends(get_db)], user_id: int,
             status_code=status.HTTP_404_NOT_FOUND,
             detail='User was not found'
         )
-#
+
     db.execute(update(User).where(User.id == user_id).values(
         firstname=update_user.firstname,
         lastname=update_user.lastname,
@@ -60,6 +75,8 @@ async def update_user(db: Annotated[Session, Depends(get_db)], user_id: int,
         'status_code': status.HTTP_200_OK,
         'transaction': 'User update is successful'
     }
+
+
 @router.delete('/delete')
 async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
     user = db.scalar(select(User).where(User.id == user_id))
@@ -69,8 +86,10 @@ async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
             detail='User was not found'
         )
     db.execute(delete(User).where(User.id == user_id))
+    db.execute(delete(Task).where(Task.user_id == user_id))
+
     db.commit()
     return {
         'status_code': status.HTTP_200_OK,
-        'transaction': 'User delete is successful'
+        'transaction': 'User and user-related tasks delete is successful'
     }
